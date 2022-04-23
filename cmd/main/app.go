ab488eb69d9df79bf18ad52585079dad5e950ae2
CryptoFreon <diautomart@gmail.com>
2022-04-24 01:57:32 +0400

    commit 1

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -53,20 +53,21 @@ func start(router *httprouter.Router, cfg *config.Config) {
 	var listenErr error
 
 	if cfg.Listen.Type == "sock" {
+		logger.Info("detect app path")
 		appDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 		if err != nil {
 			logger.Fatal(err)
 		}
 		logger.Info("create socket")
 		socketPath := path.Join(appDir, "app.sock")
-		logger.Debugf("socket path: %s", socketPath)
 
 		logger.Info("listen unix socket")
 		listener, listenErr = net.Listen("unix", socketPath)
+		logger.Infof("server is listening unix socket %s", socketPath)
 	} else {
 		logger.Info("listen tcp")
-
 		listener, listenErr = net.Listen("tcp", fmt.Sprintf("%s:%s", cfg.Listen.BindIP, cfg.Listen.Port))
+		logger.Infof("server is listening port %s:%s", cfg.Listen.BindIP, cfg.Listen.Port)
 	}
 
 	if listenErr != nil {
@@ -79,6 +80,5 @@ func start(router *httprouter.Router, cfg *config.Config) {
 		ReadTimeout:  15 * time.Second,
 	}
 
-	logger.Infof("server is listening port %s:%s", cfg.Listen.BindIP, cfg.Listen.Port)
 	logger.Fatal(server.Serve(listener))
 }
