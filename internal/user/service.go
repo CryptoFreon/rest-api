6ad464afba5c44a114b0761a98699b7e5ece3b59
CryptoFreon <diautomart@gmail.com>
2022-04-24 23:30:48 +0400

    MongoDB 1

--- internal/user/service.go
+++ internal/user/service.go
@@ -1,1 +1,16 @@
 package user
+
+import (
+	"context"
+	"rest-api/pkg/logging"
+)
+
+type Service struct {
+	storage Storage
+	logger  *logging.Logger
+}
+
+func (s *Service) Create(ctx context.Context) (u User, err error) {
+	//TODO for next one
+	return
+}
